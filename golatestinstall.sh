47e9a903f994131f0a3b8f47fe4c7bccbc0d4b39
intelfike <intelfike@gmail.com>
2017-05-01 21:22:24 +0900

    動作確認済み

--- golatestinstall.sh
+++ golatestinstall.sh
@@ -5,23 +5,26 @@ apt-get install -y wget git gcc
 
 # go1.4.3をダウンロード、解凍、削除
 # wget https://storage.googleapis.com/golang/go1.4.3.linux-amd64.tar.gz
-# mkdir go1.4
+# mkdir $HOME/go1.4
 # tar xzf go1.4.3.linux-amd64.tar.gz -C $HOME/go1.4 --strip-components 1
 # rm go1.4.3.linux-amd64.tar.gz
-git clone -b go1.4.3 https://go.googlesource.com/go
-mv go go1.4
+
+# go1.4.3をダウンロード、解凍、削除
+git clone -b go1.4.3 https://go.googlesource.com/go go1.4
+cd go1.4/src
+export CGO_ENABLED=0
+./make.bash
+export CGO_ENABLED=1
+cd ../..
 mv go1.4 $HOME
-# 環境変数を設定
-echo "
-export GOROOT_BOOTSTRAP=$HOME/go1.4" >> ~/.bashrc
-source ~/.bashrc
 
 # go最新版ダウンロード
 git clone https://go.googlesource.com/go
-cd ./go/src/
-sh all.bash
+cd go/src
+./make.bash
 cd ../..
 mv go /usr/local/
+
 # 環境変数を設定
 echo "
 export GOROOT=/usr/local/go
